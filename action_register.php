9db13f6a8732809f95e8fc7bb630bfcaf3621408
Amirmohamadsorani <amirmohamads1387@gmail.com>
2024-12-04 09:27:53 -0800

    Add files via upload

---
+++ action_register.php
@@ -0,0 +1,15 @@
+
+<?php
+include("header.html")
+$realname = $_POST["realname"]
+$username = $_POST["username"]
+$password = $_POST["password"]
+$repassward = $_POST["repassward"]
+$email = $_POST["email"]
+?>
+<?php
+include("footer.html")
+?>
+
+
+
